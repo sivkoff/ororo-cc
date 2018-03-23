51c590418c2ffb61660abcd499e30706f2f6d4d7
Vitaly Sivkov <vitaly.sivkov@gmail.com>
2018-03-23 16:46:39 +0500

    it

---
+++ README.md
@@ -0,0 +1,12 @@
+# Ororo CC toggler
+
+This script makes possible to show subtitles only when a video is paused.
+
+### Installation
+
+The easiest method is through an extension/add-on on chrome/firefox:
+
+- Chrome: install the [Tampermonkey](https://chrome.google.com/webstore/detail/dhdgffkkebhmkfjojejmpbldmpobfkfo) extension
+- Firefox: install the [Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/) add-on
+
+Then add the script using the following link: https://github.com/sivkoff/ororo-cc/raw/master/main.js
---
+++ main.js
@@ -0,0 +1,55 @@
+// ==UserScript==
+// @name           Ororo CC toggler
+// @namespace      https://github.com/sivkoff
+// @description    Makes possible to show subtitles only when a video is paused
+// @match          https://ororo.tv/*/*
+// @version        0.1
+// @updateURL      https://github.com/sivkoff/ororo-cc/raw/master/main.js
+// @downloadURL    https://github.com/sivkoff/ororo-cc/raw/master/main.js
+// @grant          none
+// ==/UserScript==
+
+
+function getVideo() {
+  return document.querySelector('video.vjs-tech');
+}
+
+function initScript() {
+  const cc = document.querySelector('.vjs-text-track-display');
+  const playButton = document.querySelector('.vjs-control-bar .vjs-play-control');
+  const video = getVideo();
+
+  const isPlaying = () => Array.from(playButton.classList).includes('vjs-paused');
+
+  let play = isPlaying();
+  const toggleCC = () => {
+    play = !play;
+    cc.style.display = play ? 'none' : 'inherit';
+  };
+  const keyboardHandler = (event) => {
+    if (event.keyCode !== 32) return;
+
+    toggleCC();
+  };
+
+
+  document.body.addEventListener('keydown', keyboardHandler);
+  video.addEventListener('click', toggleCC);
+  playButton.addEventListener('click', toggleCC);
+  toggleCC();
+}
+
+window.addEventListener('load', () => {
+  let isOpen = false;
+
+  setInterval(() => {
+    const video = getVideo();
+
+    if (!video && isOpen) {
+      isOpen = false;
+    } else if (video && !isOpen) {
+      isOpen = true;
+      initScript();
+    }
+  });
+});
